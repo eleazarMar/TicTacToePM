4df82e82771a82ffbc8b83559ea4610d7f5eb5e8
p.m.class <e>
2021-01-26 13:01:23 -0600

    Grid layout is done

--- TicTacToePM/ContentView.swift
+++ TicTacToePM/ContentView.swift
@@ -21,25 +21,41 @@ struct Home :  View {
     var body: some View {
         VStack {
             
+            
+            
             LazyVGrid(columns:Array(repeating:GridItem(.flexible(),spacing: 15), count: 3),spacing: 15) {
                 
                 ForEach(0..<9, id: \.self) {index in
                     
                     Color.pink
+                        .frame(width: getWidth(), height: getWidth())
+                        .cornerRadius(30)
+               
                 }
             }
+           
             .padding(15)
             
         }
+        
+    }
+    
+
+    
+    
+    
+    
+    //Used to calculate width.
+    
+    func getWidth() -> CGFloat {
+        let width = UIScreen.main.bounds.width - (30 + 30)
+        
+        return width / 3
+    }
+
+    struct ContentView_Previews: PreviewProvider {
+        static var previews: some View {
+            ContentView()
+        }
     }
 }
-
-
-            
-
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
-
