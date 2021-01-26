ec9328627e3d153e8fe88e4350577c7c6fa69929
p.m.class <e>
2021-01-26 12:49:12 -0600

    Finished basic grid.

--- TicTacToePM/ContentView.swift
+++ TicTacToePM/ContentView.swift
@@ -9,13 +9,37 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+       
+        NavigationView {
+        Home()
+            .navigationTitle("Tic Tac Toe")
+            .preferredColorScheme(/*@START_MENU_TOKEN@*/.dark/*@END_MENU_TOKEN@*/)
+        }
     }
 }
+struct Home :  View {
+    var body: some View {
+        VStack {
+            
+            LazyVGrid(columns:Array(repeating:GridItem(.flexible(),spacing: 15), count: 3),spacing: 15) {
+                
+                ForEach(0..<9, id: \.self) {index in
+                    
+                    Color.pink
+                }
+            }
+            .padding(15)
+            
+        }
+    }
+}
+
+
+            
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
 }
+
